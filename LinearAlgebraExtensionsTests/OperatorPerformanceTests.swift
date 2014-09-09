6cade03eaaed4a9d37d9c23222e1c128ac6ef774
Damien Pontifex <damien.pontifex@gmail.com>
2014-09-09 15:00:32 +0800

    Refactoring performance tests

---
+++ LinearAlgebraExtensionsTests/OperatorPerformanceTests.swift
@@ -0,0 +1,139 @@
+//
+//  OperatorPerformanceTests.swift
+//  LinearAlgebraExtensions
+//
+//  Created by Damien Pontifex on 9/09/2014.
+//  Copyright (c) 2014 Damien Pontifex. All rights reserved.
+//
+
+import XCTest
+import Accelerate
+import LinearAlgebraExtensions
+
+class OperatorPerformanceTests: XCTestCase {
+	
+	// Pure la matrix operations for standard
+	func testPerformanceExample() {
+		let squareDim = 1_024
+		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
+		let initial = 0.0
+		
+		let rowDim = la_count_t(squareDim)
+		
+		var array = [Double](count: elementCount, repeatedValue: initial)
+		
+		for i in 0..<elementCount {
+			array[i] = Double(arc4random_uniform(100))
+		}
+		
+		measureBlock {
+			var matrix = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+			var matrix2 = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+			
+			let result = la_matrix_product(matrix, matrix2)
+			
+			// Have to actually call it out for execution to occur
+			var outArray = Array<Double>(count: elementCount, repeatedValue: 0.0)
+			let status = la_matrix_to_double_buffer(&outArray, rowDim, result)
+		}
+	}
+	
+	// Performance with custom operator for matrix multiplication
+	// Rest standard pure la functions
+	func testPerformanceWithMultiplierOperator() {
+		let squareDim = 1_024
+		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
+		let initial = 0.0
+		
+		let rowDim = la_count_t(squareDim)
+		
+		var array = [Double](count: elementCount, repeatedValue: initial)
+		
+		for i in 0..<elementCount {
+			array[i] = Double(arc4random_uniform(100))
+		}
+		
+		measureBlock {
+			var matrix = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+			var matrix2 = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+			
+			let result = matrix * matrix2
+			
+			// Have to actually call it out for execution to occur
+			var array = Array<Double>(count: elementCount, repeatedValue: 0.0)
+			let status = la_matrix_to_double_buffer(&array, rowDim, result)
+		}
+	}
+	
+	// Performance with helper method to convert back to Swift array
+	// Rest standard pure la functions
+	func testToArrayPerformance() {
+		let squareDim = 1_024
+		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
+		let initial = 0.0
+		
+		let rowDim = la_count_t(squareDim)
+		
+		var array = [Double](count: elementCount, repeatedValue: initial)
+		
+		for i in 0..<elementCount {
+			array[i] = Double(arc4random_uniform(100))
+		}
+		
+		var matrix = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+		var matrix2 = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+		
+		let result = la_matrix_product(matrix, matrix2)
+		
+		measureBlock {
+			// Have to actually call it out for execution to occur
+			var array = result.toArray()
+		}
+	}
+	
+	// Performance with all custom functions for multiplication and array conversion
+	func testAllExtensionFunctionsCombined() {
+		let squareDim = 1_024
+		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
+		let initial = 0.0
+		
+		let rowDim = la_count_t(squareDim)
+		
+		var array = [Double](count: elementCount, repeatedValue: initial)
+		
+		for i in 0..<elementCount {
+			array[i] = Double(arc4random_uniform(100))
+		}
+		
+		var matrix = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+		var matrix2 = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
+		
+		measureBlock {
+			let result = matrix * matrix2
+			
+			// Have to actually call it out for execution to occur
+			var array = result.toArray()
+		}
+	}
+	
+	// testing vDSP for comparison to Linear Algebra framework
+	func testVSPPerformance() {
+		let squareDim = 1_024
+		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
+		let initial = 0.0
+		
+		let rowDim = vDSP_Stride(squareDim)
+		let length = vDSP_Length(squareDim)
+		
+		var array = [Double](count: elementCount, repeatedValue: initial)
+		
+		for i in 0..<elementCount {
+			array[i] = Double(arc4random_uniform(100))
+		}
+		
+		measureBlock {
+			var vsresult = [Double](count : array.count, repeatedValue : 0.0)
+			vDSP_mmulD(&array, 1, &array, 1, &vsresult, 1, length, length, length)
+		}
+	}
+}
--- LinearAlgebraExtensionsTests/OperatorTests.swift
+++ LinearAlgebraExtensionsTests/OperatorTests.swift
@@ -12,17 +12,7 @@ import LinearAlgebraExtensions
 
 class OperatorTests: XCTestCase {
 	
-	override func setUp() {
-		super.setUp()
-		// Put setup code here. This method is called before the invocation of each test method in the class.
-	}
-	
-	override func tearDown() {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
-		super.tearDown()
-	}
-	
-	func testMatrixMultiply() {
+	func testMatrixMultiplyOperator() {
 		let twoDArray = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
 		var matrix = la_object_t.objectFromArray(twoDArray) as la_object_t
 		var matrix2 = la_object_t.objectFromArray(twoDArray) as la_object_t
@@ -37,7 +27,7 @@ class OperatorTests: XCTestCase {
 		XCTAssertEqual(result[0], 30, "Should have 30 as result of first element")
 	}
 	
-	func testScalarAdd() {
+	func testMatrixAddOperator() {
 		let rows = 3
 		let cols = 3
 		
@@ -49,6 +39,7 @@ class OperatorTests: XCTestCase {
 		
 		var result = sumMatrix.toArray()
 		
+		// Verify
 		for (y, array) in enumerate(twoDArray) {
 			for (x, array) in enumerate(array) {
 				
@@ -57,107 +48,6 @@ class OperatorTests: XCTestCase {
 				
 				XCTAssertEqual(originalElement, resultElement, "Should have equal elements at (\(x), \(y))")
 			}
-		}
-	}
-	
-	func testPerformanceExample() {
-		
-		let squareDim = 1_024
-		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
-		let initial = 0.0
-		
-		let rowDim = la_count_t(squareDim)
-		
-		var array = Array<Double>(count: elementCount, repeatedValue: initial)
-		
-		for i in 0..<elementCount {
-			array[i] = Double(arc4random_uniform(100))
-		}
-		
-		measureBlock {
-			var matrix = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
-			var matrix2 = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
-			
-			let result = la_matrix_product(matrix, matrix2)
-			
-			// Have to actually call it out for execution to occur
-			var array = Array<Double>(count: elementCount, repeatedValue: 0.0)
-			let status = la_matrix_to_double_buffer(&array, rowDim, result)
-		}
-	}
-	
-	func testPerformanceWithMultiplierOperator() {
-		let squareDim = 1_024
-		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
-		let initial = 0.0
-		
-		let rowDim = la_count_t(squareDim)
-		
-		var array = [Double](count: elementCount, repeatedValue: initial)
-		
-		for i in 0..<elementCount {
-			array[i] = Double(arc4random_uniform(100))
-		}
-		
-		measureBlock {
-			var matrix = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
-			var matrix2 = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
-			
-			let result = matrix * matrix2
-			
-			// Have to actually call it out for execution to occur
-			var array = Array<Double>(count: elementCount, repeatedValue: 0.0)
-			let status = la_matrix_to_double_buffer(&array, rowDim, result)
-		}
-	}
-	
-	func testToArrayPerformance() {
-		//TODO: last run was 2.707s 1% STDEV!! Needs work on toArray() method
-		
-		let squareDim = 1_024
-		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
-		let initial = 0.0
-		
-		let rowDim = la_count_t(squareDim)
-		
-		var array = [Double](count: elementCount, repeatedValue: initial)
-		
-		for i in 0..<elementCount {
-			array[i] = Double(arc4random_uniform(100))
-		}
-		
-		var matrix = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
-		var matrix2 = la_matrix_from_double_buffer(&array, rowDim, rowDim, rowDim, 0, 0)
-		
-		let result = matrix * matrix2
-		
-		measureBlock {
-			// Have to actually call it out for execution to occur
-			var array = result.toArray()
-		}
-	}
-	
-	func testVSPPerformance() {
-		let squareDim = 1_024
-		let elementCount = Int(pow(Double(squareDim), Double(2.0)))
-		let initial = 0.0
-		
-		let rowDim = vDSP_Stride(squareDim)
-		let length = vDSP_Length(squareDim)
-		
-		var array = [Double](count: elementCount, repeatedValue: initial)
-		
-		for i in 0..<elementCount {
-			array[i] = Double(arc4random_uniform(100))
-		}
-		
-		var m1 = [ 3.0, 2.0, 4.0, 5.0, 6.0, 7.0 ]
-		var m2 = [ 10.0, 20.0, 30.0, 30.0, 40.0, 50.0]
-		var mresult = [Double](count : 9, repeatedValue : 0.0)
-		
-		measureBlock {
-			var vsresult = [Double](count : array.count, repeatedValue : 0.0)
-			vDSP_mmulD(&array, 1, &array, 1, &vsresult, 1, length, length, length)
 		}
 	}
 }
